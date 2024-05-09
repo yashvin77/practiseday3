a5fea50837f256f5dd9bbcde05e8d125c000bd7c
yaswanth <amara.yaswanth77@gmail.com>
2024-05-09 20:12:28 +0530

    changes

--- day-3/main.tf
+++ day-3/main.tf
@@ -50,64 +50,62 @@ resource "aws_route_table_association" "custom_VPC" {
   subnet_id = aws_subnet.custom_VPC.id
 }
 
-# security group creation
-resource "aws_security_group" "custom_VPC" {
- 
-  vpc_id = aws_vpc.custom_VPC.id
-  name = "cust-sg"
-
-}
 
 # security group ingress 
 
-resource "aws_vpc_security_group_ingress_rule" "custom_VPC" {
+resource "aws_security_group" "dev" {
+  name        = "allow_tls"
+  vpc_id      = aws_vpc.custom_VPC.id
+  tags = {
+    Name = "dev_sg"
+  }
+ ingress {
+    description      = "TLS from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "TCP"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "TCP"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
 
-    security_group_id = aws_security_group.custom_VPC.id
-  from_port         = 443
-  ip_protocol       = "tcp"
-  to_port           = 443
-  cidr_ipv4 = "0.0.0.0/0"
-}
 
-resource "aws_vpc_security_group_ingress_rule" "cus" {
+egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
 
-    security_group_id = aws_security_group.custom_VPC.id
-  from_port         = 80
-  ip_protocol       = "tcp"
-  to_port           = 80
-  cidr_ipv4 = "0.0.0.0/0"
-}
 
-resource "aws_vpc_security_group_ingress_rule" "cust" {
+  }
 
-    security_group_id = aws_security_group.custom_VPC.id
-  from_port         = 22
-  ip_protocol       = "tcp"
-  to_port           = 22
-  cidr_ipv4 = "0.0.0.0/0"
-}
-
-# security group outbound
-
-resource "aws_vpc_security_group_egress_rule" "cu" {
-
-    security_group_id = aws_security_group.custom_VPC.id
-  
-     ip_protocol = "-1"
-    cidr_ipv4 = "0.0.0.0/0"
-  
-}
 
   resource "aws_instance" "ec2" {
     ami = var.ami
     instance_type = var.instance_type
     key_name = var.key_name
     subnet_id = aws_subnet.custom_VPC.id
-    security_groups = [aws_security_group.custom_VPC.id]
-
+    security_groups = ["aws_security_group.dev.id"]
     tags = {
       
-      Name = "myec7"
+      Name = "myec6"
     }
 
   }
